1094b36851c766b8674dd0f4710e2ca1e2aa5c89
sehrish255 <sehrishhoodbhoy25@gmail.com>
2021-09-23 19:19:33 -0400

    fixing conflict

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", initializeApp);
+    function initializeApp(){
+        displayMeme();
+    }
+
+    async function displayMeme(){
+        try{
+            const response=await
+            fetch("https://api.imgflip.com/get_memes");
+            const jsonResponse = await response.json();
+            //required
+            //jsonResponse is object
+            console.log(jsonResponse);
+            const meme = jsonResponse.data.memes[1];
+
+            //creating an element and assigning src image and name and width
+            const image = document.createElement("img");
+            image.src=meme.url;
+            image.alt= meme.name;
+            image.width=200;
+
+            //grabbing meme which is id and assigning to variable memeContainer, to add image to meme container 
+            const memeContainer=
+                document.querySelector("#meme");
+            memeContainer.appendChild(image);
+        }catch (error){
+            console.log("promise failed", error);
+        }
+    }
